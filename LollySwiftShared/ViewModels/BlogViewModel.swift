0c6f968f705f659a3909f67c28b7eeab2a136baa
趙偉 <zwvista@msn.com>
2019-01-06 19:33:15 +0900

    marked

--- LollySwiftShared/ViewModels/BlogViewModel.swift
+++ LollySwiftShared/ViewModels/BlogViewModel.swift
@@ -7,34 +7,55 @@
 //
 
 import Foundation
+import Regex
 
 class BlogViewModel: NSObject {
+    private func html1With(_ s: String) -> String {
+        return "<strong><span style=\"color:#0000ff;\">\(s)</span></strong>"
+    }
+    private func htmlWordWith(_ s: String) -> String { return html1With(s + "：") }
+    private func htmlBWith(_ s: String) -> String { return html1With(s) }
+    // tag for explantion 1
+    private func htmlE1With(_ s: String) -> String {
+        return "<span style=\"color:#006600;\">\(s)</span>"
+    }
+    private func html2With(_ s: String) -> String {
+        return "<span style=\"color:#cc00cc;\">\(s)</span>"
+    }
+    // tag for explantion 2
+    private func htmlE2With(_ s: String) -> String { return html2With(s) }
+    private func htmlIWith(_ s: String) -> String { return "<strong>\(html2With(s))</strong>" }
+    private let regMarkedEntry = "(\\*\\*?)\\s*(.*?)：(.*?)：(.*)".r!
+    private let regMarkedB = "<B>(.+?)</B>".r!
+    private let regMarkedI = "<I>(.+?)</I>".r!
+    private var regHtmlB: Regex { return htmlBWith("(.+?)").r! }
+    private var regHtmlI: Regex { return htmlIWith("(.+?)").r! }
+    private var htmlEntry: String { return "\(htmlWordWith("(.*?)"))(?:\(htmlE1With("(.*?)")))?(?:\(htmlE2With("(.*?)")))?" }
+    private var regHtmlEntry1: Regex { return "<li>\(htmlEntry)</li>".r! }
+    private var regHtmlEntry2: Regex { return "<br>\(htmlEntry)".r! }
 
-    public func markedToHtml(text: String) -> String {
+    func markedToHtml(text: String) -> String {
         var arr = text.components(separatedBy: "\n")
-        let reg = "(\\*\\*?)\\s*(.*?)：(.*?)：(.*)".r!
         var i = 0
         while i < arr.count {
             var s = arr[i]
-            if let m = reg.findFirst(in: s) {
+            if let m = regMarkedEntry.findFirst(in: s) {
                 let (s1, s2, s3, s4) = (m.group(at: 1)!, m.group(at: 2)!, m.group(at: 3)!, m.group(at: 4))
-                s = "<strong><span style=\"color:#0000ff;\">\(s2)：</span></strong>" +
-                    (s3.isEmpty ? "" : "<span style=\"color:#006600;\">\(s3)</span>") +
-                    ((s4 ?? "").isEmpty ? "" : "<span style=\"color:#cc00cc;\">\(s4!)</span>")
+                s = htmlWordWith(s2) + (s3.isEmpty ? "" : htmlE1With(s3)) + ((s4 ?? "").isEmpty ? "" : htmlE2With(s4!))
                 arr[i] = s1 == "*" ? "<li>\(s)</li>" : "<br>" + s
                 if (i == 0 || arr[i - 1].hasPrefix("<div>")) {
                     arr.insert("<ul>", at: i)
                     i += 1
                 }
-                if i == arr.count - 1 || !reg.matches(arr[i + 1]) {
+                if i == arr.count - 1 || !regMarkedEntry.matches(arr[i + 1]) {
                     arr.insert("</ul>", at: i + 1)
                     i += 1
                 }
             } else if s.isEmpty {
                 arr[i] = "<div><br></div>"
             } else {
-                s = "<B>(.+?)</B>".r!.replaceAll(in: s, with: "<strong><span style=\"color:#0000ff;\">$1</span></strong>")
-                s = "<I>(.+?)</I>".r!.replaceAll(in: s, with: "<strong><span style=\"color:#cc00cc;\">$1</span></strong>")
+                s = regMarkedB.replaceAll(in: s, with: htmlBWith("$1"))
+                s = regMarkedI.replaceAll(in: s, with: htmlIWith("$1"))
                 arr[i] = "<div>\(s)</div>"
             }
             i += 1
@@ -42,7 +63,7 @@ class BlogViewModel: NSObject {
         return arr.joined(separator: "\n")
     }
     
-    public func htmlToMarked(text: String) -> String {
+    func htmlToMarked(text: String) -> String {
         var arr = text.split("\n")
         var i = 0
         while i < arr.count {
@@ -54,12 +75,16 @@ class BlogViewModel: NSObject {
                 arr[i] = ""
             } else if let m = "<div>(.*?)</div>".r!.findFirst(in: s) {
                 s = m.group(at: 1)!
-                s = "<strong><span style=\"color:#0000ff;\">(.+?)</span></strong>".r!.replaceAll(in: s, with: "<B>$1</B>")
-                s = "<strong><span style=\"color:#cc00cc;\">(.+?)</span></strong>".r!.replaceAll(in: s, with: "<I>$1</I>")
+                s = regHtmlB.replaceAll(in: s, with: "<B>$1</B>")
+                s = regHtmlI.replaceAll(in: s, with: "<I>$1</I>")
                 arr[i] = s
-            } else if let m = "<li><strong><span style=\"color:#0000ff;\">(.*?)：</span></strong>(?:<span style=\"color:#006600;\">(.*?)</span>)?(?:<span style=\"color:#cc00cc;\">(.*?)</span>)?</li>".r!.findFirst(in: s) {
+            } else if let m = regHtmlEntry1.findFirst(in: s) {
                 let (s1, s2, s3) = (m.group(at: 1), m.group(at: 2), m.group(at: 3))
                 s = "* \(s1 ?? "")：\(s2 ?? "")：\(s3 ?? "")"
+                arr[i] = s
+            } else if let m = regHtmlEntry2.findFirst(in: s) {
+                let (s1, s2, s3) = (m.group(at: 1), m.group(at: 2), m.group(at: 3))
+                s = "** \(s1 ?? "")：\(s2 ?? "")：\(s3 ?? "")"
                 arr[i] = s
             }
             i += 1
@@ -67,23 +92,27 @@ class BlogViewModel: NSObject {
         return arr.joined(separator: "\n")
     }
     
-    public func addTagB(text: String) -> String {
+    func addTagB(text: String) -> String {
         return "<B>\(text)</B>"
     }
-    public func addTagI(text: String) -> String {
+    func addTagI(text: String) -> String {
         return "<I>\(text)</I>"
     }
-    public func removeTags(text: String) -> String {
+    func removeTags(text: String) -> String {
         return "</?[BI]>".r!.replaceAll(in: text, with: "")
     }
-    public let explanation = "* ：：\n"
-    public func getHtml(text: String) -> String {
+    let explanation = "* ：：\n"
+    func getHtml(text: String) -> String {
         return "<html><body>\(text)</body></html>"
     }
-    public func getPatternUrl(patternNo: String) -> String {
+    func getPatternUrl(patternNo: String) -> String {
         return "http://viethuong.web.fc2.com/MONDAI/\(patternNo).html"
     }
-    public func getPatternMarkDown(patternText: String) -> String {
+    func getPatternMarkDown(patternText: String) -> String {
         return "* [\(patternText)　文法](https://www.google.com/search?q=\(patternText)　文法)\n* [\(patternText)　句型](https://www.google.com/search?q=\(patternText)　句型)"
     }
+    
+//    func getNotes(text: String) -> String {
+//
+//    }
 }
