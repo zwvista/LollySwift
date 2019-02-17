915bdcc97c441d7e49b17a101f3bd44dea5569c7
趙偉 <zwvista@msn.com>
2019-02-17 22:30:11 +0900

    fami

--- LollySwiftShared/Models/MWordFami.swift
+++ LollySwiftShared/Models/MWordFami.swift
@@ -1,5 +1,5 @@
 //
-//  MWordsFami.swift
+//  MWordFami.swift
 //  LollySwiftShared
 //
 //  Created by 趙偉 on 2016/07/05.
@@ -10,25 +10,25 @@ import Foundation
 import RxSwift
 
 @objcMembers
-class MWordsFami: NSObject, Codable {
+class MWordFami: NSObject, Codable {
     var ID = 0
     var USERID = 0
     var WORDID = 0
     var LEVEL = 0
 
-    static func getDataByUserLangWord(userid: Int, wordid: Int) -> Observable<[MWordsFami]> {
+    static func getDataByUserWord(userid: Int, wordid: Int) -> Observable<[MWordFami]> {
         // SQL: SELECT * FROM WORDSFAMI WHERE USERID=? AND WORDID=?
         let url = "\(RestApi.url)WORDSFAMI?transform=1&filter[]=USERID,eq,\(userid)&filter[]=WORDID,eq,\(wordid)"
         return RestApi.getArray(url: url, keyPath: "WORDSFAMI")
     }
 
-    static func update(item: MWordsFami) -> Observable<String> {
+    static func update(item: MWordFami) -> Observable<String> {
         // SQL: UPDATE WORDSFAMI SET USERID=?, WORDID=?, LEVEL=? WHERE ID=?
         let url = "\(RestApi.url)WORDSFAMI/\(item.ID)"
         return RestApi.update(url: url, body: try! item.toJSONString(prettyPrint: false)!)
     }
     
-    static func create(item: MWordsFami) -> Observable<Int> {
+    static func create(item: MWordFami) -> Observable<Int> {
         // SQL: INSERT INTO WORDSFAMI (USERID, WORDID, LEVEL) VALUES (?,?,?)
         let url = "\(RestApi.url)WORDSFAMI"
         return RestApi.create(url: url, body: try! item.toJSONString(prettyPrint: false)!).map {
