c07c231dc2c24ee07afdfa6a0b0bc5d8002b571d
趙偉 <zwvista@msn.com>
2019-05-22 15:17:53 +0900

    review

--- LollySwiftiOS/LollySwiftiOS/Phrases/PhrasesReviewViewController.swift
+++ LollySwiftiOS/LollySwiftiOS/Phrases/PhrasesReviewViewController.swift
@@ -7,24 +7,97 @@
 //
 
 import UIKit
+import RxSwift
+import AVFoundation
 
 class PhrasesReviewViewController: UIViewController {
-
+    var vm: PhrasesReviewViewModel!
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var lblPhraseTarget: UILabel!
+    @IBOutlet weak var lblIndex: UILabel!
+    @IBOutlet weak var lblCorrect: UILabel!
+    @IBOutlet weak var lblIncorrect: UILabel!
+    @IBOutlet weak var lblTranslation: UITextView!
+    @IBOutlet weak var tfPhraseInput: UITextField!
+    @IBOutlet weak var btnCheck: UIButton!
+    
+    var speakOrNot = false
+    var shuffled = true
+    var levelge0only = true
+    var reviewMode = 0
+    var subscription: Disposable? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        vm = PhrasesReviewViewModel(settings: vmSettings)
+        newTest(self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        subscription?.dispose()
     }
-    */
 
+    private func doTest() {
+        let b = vm.hasNext()
+        lblIndex.isHidden = !b
+        lblCorrect.isHidden = true
+        lblIncorrect.isHidden = true
+        btnCheck.isEnabled = b
+        btnCheck.titleLabel?.text = vm.isTestMode ? "Check" : "Next"
+        lblPhraseTarget.text = vm.isTestMode ? "" : vm.currentPhrase
+        lblTranslation.text = ""
+        tfPhraseInput.text = ""
+        tfPhraseInput.becomeFirstResponder()
+        if b {
+            lblIndex.text = "\(vm.index + 1)/\(vm.arrPhrases.count)"
+            if speakOrNot {
+                let utterance = AVSpeechUtterance(string: vm.currentPhrase)
+                utterance.voice = AVSpeechSynthesisVoice(identifier: vmSettings.selectediOSVoice.VOICENAME)
+                AppDelegate.synth.speak(utterance)
+            }
+        } else {
+            subscription?.dispose()
+        }
+    }
+    
+    @IBAction func newTest(_ sender: AnyObject) {
+        subscription?.dispose()
+        vm.newTest(mode: ReviewMode(rawValue: reviewMode)!, shuffled: shuffled).subscribe {
+            self.doTest()
+        }.disposed(by: disposeBag)
+        if vm.mode == .reviewAuto {
+            subscription = Observable<Int>.interval(vmSettings.USREVIEWINTERVAL.toDouble / 1000.0, scheduler: MainScheduler.instance).subscribe { _ in
+                self.check(self)
+            }
+            subscription?.disposed(by: disposeBag)
+        }
+    }
+    
+    @IBAction func check(_ sender: AnyObject) {
+        if !vm.isTestMode {
+            vm.next()
+            doTest()
+        } else if lblCorrect.isHidden && lblIncorrect.isHidden {
+            tfPhraseInput.text = vmSettings.autoCorrectInput(text: tfPhraseInput.text!)
+            lblPhraseTarget.isHidden = false
+            lblPhraseTarget.text = vm.currentPhrase
+            if tfPhraseInput.text == vm.currentPhrase {
+                lblCorrect.isHidden = false
+            } else {
+                lblIncorrect.isHidden = false
+            }
+            btnCheck.titleLabel?.text = "Next"
+            vm.check(phraseInput: tfPhraseInput.text!)
+        } else {
+            vm.next()
+            doTest()
+            btnCheck.titleLabel?.text = "Check"
+        }
+    }
+    
+    
+    deinit {
+        print("DEBUG: \(self.className) deinit")
+    }
 }
--- LollySwiftiOS/LollySwiftiOS/Words/WordsReviewViewController.swift
+++ LollySwiftiOS/LollySwiftiOS/Words/WordsReviewViewController.swift
@@ -7,24 +7,103 @@
 //
 
 import UIKit
+import RxSwift
+import AVFoundation
 
 class WordsReviewViewController: UIViewController {
+    var vm: WordsReviewViewModel!
+    let disposeBag = DisposeBag()
+
+    @IBOutlet weak var lblWordTarget: UILabel!
+    @IBOutlet weak var lblIndex: UILabel!
+    @IBOutlet weak var lblCorrect: UILabel!
+    @IBOutlet weak var lblIncorrect: UILabel!
+    @IBOutlet weak var lblAccuracy: UILabel!
+    @IBOutlet weak var tvTranslation: UITextView!
+    @IBOutlet weak var tfWordInput: UITextField!
+    @IBOutlet weak var btnCheck: UIButton!
+    
+    var speakOrNot = false
+    var shuffled = true
+    var levelge0only = true
+    var reviewMode = 0
+    var subscription: Disposable? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        vm = WordsReviewViewModel(settings: vmSettings)
+        newTest(self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        subscription?.dispose()
     }
-    */
 
+    private func doTest() {
+        let b = vm.hasNext()
+        lblIndex.isHidden = !b
+        lblCorrect.isHidden = true
+        lblIncorrect.isHidden = true
+        lblAccuracy.isHidden = !vm.isTestMode || !b
+        btnCheck.isEnabled = b
+        btnCheck.titleLabel?.text = vm.isTestMode ? "Check" : "Next"
+        lblWordTarget.text = vm.isTestMode ? "" : vm.currentItem?.WORDNOTE ?? ""
+        tvTranslation.text = ""
+        tfWordInput.text = ""
+        tfWordInput.becomeFirstResponder()
+        if b {
+            lblIndex.text = "\(vm.index + 1)/\(vm.arrWords.count)"
+            lblAccuracy.text = vm.currentItem!.ACCURACY
+            if speakOrNot {
+                let utterance = AVSpeechUtterance(string: vm.currentWord)
+                utterance.voice = AVSpeechSynthesisVoice(identifier: vmSettings.selectediOSVoice.VOICENAME)
+                AppDelegate.synth.speak(utterance)
+            }
+            vm.getTranslation().subscribe(onNext: {
+                self.tvTranslation.text = $0
+            }).disposed(by: disposeBag)
+        } else {
+            subscription?.dispose()
+        }
+    }
+
+    @IBAction func newTest(_ sender: AnyObject) {
+        subscription?.dispose()
+        vm.newTest(mode: ReviewMode(rawValue: reviewMode)!, shuffled: shuffled, levelge0only: levelge0only).subscribe {
+            self.doTest()
+        }.disposed(by: disposeBag)
+        if vm.mode == .reviewAuto {
+            subscription = Observable<Int>.interval(vmSettings.USREVIEWINTERVAL.toDouble / 1000.0, scheduler: MainScheduler.instance).subscribe { _ in
+                self.check(self)
+            }
+            subscription?.disposed(by: disposeBag)
+        }
+    }
+    
+    @IBAction func check(_ sender: AnyObject) {
+        if !vm.isTestMode {
+            vm.next()
+            doTest()
+        } else if lblCorrect.isHidden && lblIncorrect.isHidden {
+            tfWordInput.text = vmSettings.autoCorrectInput(text: tfWordInput.text!)
+            lblWordTarget.isHidden = false
+            lblWordTarget.text = vm.currentWord
+            if tfWordInput.text == vm.currentWord {
+                lblCorrect.isHidden = false
+            } else {
+                lblIncorrect.isHidden = false
+            }
+            btnCheck.titleLabel?.text = "Next"
+            vm.check(wordInput: tfWordInput.text!).subscribe().disposed(by: disposeBag)
+        } else {
+            vm.next()
+            doTest()
+            btnCheck.titleLabel?.text = "Check"
+        }
+    }
+
+
+    deinit {
+        print("DEBUG: \(self.className) deinit")
+    }
 }
