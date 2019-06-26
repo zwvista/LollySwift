0c9c5e188a85b2ff32d940067a84f71d35752405
趙偉 <zwvista@msn.com>
2019-06-26 11:14:47 +0900

    review options

---
+++ LollySwiftMac/LollySwiftMac/Tools/ReviewOptionsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewOptionsViewController.swift
+//  LollySwiftMac
+//
+//  Created by 趙偉 on 2019/06/26.
+//  Copyright © 2019 趙偉. All rights reserved.
+//
+
+import Cocoa
+
+class ReviewOptionsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
