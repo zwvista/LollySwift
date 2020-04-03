273018d82c73c33f15d6d0ba0512bf147b843398
趙偉 <zwvista@msn.com>
2020-04-03 09:32:09 +0900

    WordsDict

---
+++ LollySwiftMac/LollySwiftMac/Words/WordsDictViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WordsDictViewController.swift
+//  LollySwiftMac
+//
+//  Created by 趙偉 on 2020/04/02.
+//  Copyright © 2020 趙偉. All rights reserved.
+//
+
+import Cocoa
+
+class WordsDictViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
