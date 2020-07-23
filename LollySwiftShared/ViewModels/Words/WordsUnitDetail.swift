1e4c30a127a7c2d02b819e9676a39da447810f6d
趙偉 <zwvista@msn.com>
2020-07-23 14:45:08 +0900

    models

---
+++ LollySwiftShared/ViewModels/Words/WordsUnitDetail.swift
@@ -0,0 +1,13 @@
+//
+//  WordsUnitDetail.swift
+//  LollySwiftMac
+//
+//  Created by 趙偉 on 2020/07/21.
+//  Copyright © 2020 趙偉. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class WordsUnitDetailViewModel: NSObject {
+}
