ff5fbf32bffe28d149fca530907544db8308f029
趙偉 <zwvista@msn.com>
2020-11-30 07:52:48 +0900

    swiftUI

---
+++ LollySwiftUIMac/LollySwiftUIMac/LollySwiftUIMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LollySwiftUIMacApp.swift
+//  LollySwiftUIMac
+//
+//  Created by 趙偉 on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct LollySwiftUIMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LollySwiftUIiOS/LollySwiftUIiOS/LollySwiftUIiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LollySwiftUIiOSApp.swift
+//  LollySwiftUIiOS
+//
+//  Created by 趙偉 on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct LollySwiftUIiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
