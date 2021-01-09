eb29717b63ca7781d8902973f132a0c4a16526ea
趙偉 <zwvista@msn.com>
2021-01-09 21:17:41 +0900

    dictStore 4

---
+++ LollySwiftShared/Shared/DictStoreUI.swift
@@ -0,0 +1,70 @@
+//
+//  DictStoreUI.swift
+//  LollySwiftiOS
+//
+//  Created by 趙　偉 on 2021/01/08.
+//  Copyright © 2021 趙 偉. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+import NSObject_Rx
+
+class DictStoreUI: NSObject, ObservableObject {
+    
+    @Published
+    var dictStatus = DictWebViewStatus.ready
+    @Published
+    var word = ""
+    @Published
+    var dict: MDictionary!
+    var url = ""
+    
+    var vmSettings: SettingsViewModel!
+    weak var wvDict: WKWebView!
+    
+    func searchDict() {
+        url = dict.urlString(word: word, arrAutoCorrect: vmSettings.arrAutoCorrect)
+        dictStatus = .ready
+        if dict.DICTTYPENAME == "OFFLINE" {
+            wvDict.load(URLRequest(url: URL(string: "about:blank")!))
+            RestApi.getHtml(url: url).subscribe(onNext: { html in
+                print(html)
+                let str = self.dict.htmlString(html, word: self.word)
+                self.wvDict.loadHTMLString(str, baseURL: nil)
+            }) ~ rx.disposeBag
+        } else {
+            wvDict.load(URLRequest(url: URL(string: url)!))
+            if !dict.AUTOMATION.isEmpty {
+                dictStatus = .automating
+            } else if dict.DICTTYPENAME == "OFFLINE-ONLINE" {
+                dictStatus = .navigating
+            }
+        }
+    }
+
+    func onNavigationFinished() {
+        //        guard webView.stringByEvaluatingJavaScript(from: "document.readyState") == "complete" && status == .navigating else {return}
+        guard dictStatus != .ready else {return}
+        switch dictStatus {
+        case .automating:
+            let s = dict.AUTOMATION.replacingOccurrences(of: "{0}", with: word)
+            wvDict.evaluateJavaScript(s) { (html: Any?, error: Error?) in
+                self.dictStatus = .ready
+                if self.dict.DICTTYPENAME == "OFFLINE-ONLINE" {
+                    self.dictStatus = .navigating
+                }
+            }
+        case .navigating:
+            // https://stackoverflow.com/questions/34751860/get-html-from-wkwebview-in-swift
+            wvDict.evaluateJavaScript("document.documentElement.outerHTML.toString()") { (html: Any?, error: Error?) in
+                let html = html as! String
+                print(html)
+                let str = self.dict.htmlString(html, word: self.word)
+                self.wvDict.loadHTMLString(str, baseURL: nil)
+                self.dictStatus = .ready
+            }
+        default: break
+        }
+    }
+}
--- LollySwiftUIiOS/LollySwiftUIiOS/ContentView.swift
+++ LollySwiftUIiOS/LollySwiftUIiOS/ContentView.swift
@@ -11,15 +11,12 @@ import WebKit
 
 struct ContentView: View {
     @ObservedObject var vm = vmSettings
-    @State var word = ""
     @ObservedObject var webViewStore = WebViewStore()
-    @State var status = DictWebViewStatus.ready
-    let disposeBag = DisposeBag()
+    @ObservedObject var dictStore = DictStoreUI()
     var wvDict: WKWebView { webViewStore.webView }
-    @State var dictStatus = DictWebViewStatus.ready
     var body: some View {
         VStack {
-            SearchBar(text: $word, placeholder: "Word") {_ in }
+            SearchBar(text: $dictStore.word, placeholder: "Word") {_ in dictStore.searchDict() }
             HStack {
                 // https://stackoverflow.com/questions/59348093/picker-for-optional-data-type-in-swiftui
                 Picker(selection: $vm.selectedDictReference, label: Text(vm.selectedDictReference?.DICTNAME ?? "Choose a Dictionary")) {
@@ -29,54 +26,17 @@ struct ContentView: View {
                 }
                 .padding().background(Color.orange)
                 .pickerStyle(MenuPickerStyle())
-                .onChange(of: vm.selectedDictReference) {_ in
-                    searchDict()
+                .onChange(of: vm.selectedDictReference) {
+                    dictStore.dict = $0
+                    dictStore.searchDict()
                 }
             }
-            WebView(webView: wvDict, onNavigationFinished: onNavigationFinished)
-        }
-    }
-    
-    func searchDict() {
-        let item = vmSettings.selectedDictReference!
-        let url = item.urlString(word: word, arrAutoCorrect: vmSettings.arrAutoCorrect)
-        if item.DICTTYPENAME == "OFFLINE" {
-            wvDict.load(URLRequest(url: URL(string: "about:blank")!))
-            RestApi.getHtml(url: url).subscribe(onNext: { html in
-                print(html)
-                let str = item.htmlString(html, word: word, useTemplate2: true)
-                wvDict.loadHTMLString(str, baseURL: nil)
-            }) ~ disposeBag
-        } else {
-            wvDict.load(URLRequest(url: URL(string: url)!))
-            if item.DICTTYPENAME == "OFFLINE-ONLINE" {
-                status = .navigating
+            WebView(webView: wvDict) {
+                dictStore.onNavigationFinished()
             }
-        }
-    }
-    
-    func onNavigationFinished() {
-        guard dictStatus != .ready else {return}
-        let item = vmSettings.selectedDictReference!
-        // https://stackoverflow.com/questions/34751860/get-html-from-wkwebview-in-swift
-        switch dictStatus {
-        case .automating:
-            let s = item.AUTOMATION.replacingOccurrences(of: "{0}", with: word)
-            wvDict.evaluateJavaScript(s) { (html: Any?, error: Error?) in
-                self.dictStatus = .ready
-                if item.DICTTYPENAME == "OFFLINE-ONLINE" {
-                    dictStatus = .navigating
-                }
-            }
-        case .navigating:
-            wvDict.evaluateJavaScript("document.documentElement.outerHTML.toString()") { (html: Any?, error: Error?) in
-                let html = html as! String
-                print(html)
-                let str = item.htmlString(html, word: word, useTemplate2: true)
-                wvDict.loadHTMLString(str, baseURL: nil)
-                dictStatus = .ready
-            }
-        default: break
+        }.onAppear {
+            dictStore.vmSettings = vmSettings
+            dictStore.wvDict = wvDict
         }
     }
 }
