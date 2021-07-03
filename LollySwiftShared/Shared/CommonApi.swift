758541c0720e1ca1ebe0e17fb120d0c19ff4d328
趙偉 <zwvista@msn.com>
2021-07-03 18:58:38 +0900

    lolly

--- LollySwiftShared/Shared/CommonApi.swift
+++ LollySwiftShared/Shared/CommonApi.swift
@@ -37,9 +37,9 @@ let globalUser = GlobalUser()
 
 class CommonApi {
     
-    static let urlAPI = "https://zwvista.tk/lolly/api.php/records/"
-    static let urlSP = "https://zwvista.tk/lolly/sp.php/"
-    static let cssFolder = "https://zwvista.tk/lolly/css/"
+    static let urlAPI = "https://zwvista2.tk/lolly/api.php/records/"
+    static let urlSP = "https://zwvista2.tk/lolly/sp.php/"
+    static let cssFolder = "https://zwvista2.tk/lolly/css/"
     
     static func removeReturns(html: String) -> String {
         html.replacingOccurrences(of: "\r\n", with: "\n")
