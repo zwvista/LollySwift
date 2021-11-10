e45e8ae5b96bb05d1f0afb25d4b62eeadefb7542
cho-i <cho-i@motivationworks.co.jp>
2021-11-11 01:45:14 +0900

    import RxBinding 2

---
+++ Pods/Target Support Files/RxBinding-iOS/RxBinding-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxBinding_iOS : NSObject
+@end
+@implementation PodsDummy_RxBinding_iOS
+@end
---
+++ Pods/Target Support Files/RxBinding-macOS/RxBinding-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxBinding_macOS : NSObject
+@end
+@implementation PodsDummy_RxBinding_macOS
+@end
