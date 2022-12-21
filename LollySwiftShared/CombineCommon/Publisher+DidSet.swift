c3c4fc60dc0525d51a3dc114e41058440fe19867
趙偉 <>
2022-12-22 03:33:48 +0900

    didset 4

--- LollySwiftShared/CombineCommon/Publisher+DidSet.swift
+++ LollySwiftShared/CombineCommon/Publisher+DidSet.swift
@@ -10,8 +10,11 @@ import Foundation
 import Combine
 
 // https://stackoverflow.com/questions/58403338/is-there-an-alternative-to-combines-published-that-signals-a-value-change-afte
+// https://forums.swift.org/t/is-this-a-bug-in-published/31292/16?page=2
 extension Published.Publisher {
     var didSet: AnyPublisher<Value, Never> {
-        self.receive(on: RunLoop.main).eraseToAnyPublisher()
+        self.receive(on: DispatchQueue.main).eraseToAnyPublisher()
+        // Sometimes doesn't work
+        // self.receive(on: RunLoop.main).eraseToAnyPublisher()
     }
 }
