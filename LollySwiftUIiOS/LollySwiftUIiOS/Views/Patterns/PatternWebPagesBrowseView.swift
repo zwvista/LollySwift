7d93a5467c5755c71101ea3854508211e6767325
趙偉 <>
2022-12-31 04:43:34 +0900

    uiview 21

--- LollySwiftUIiOS/LollySwiftUIiOS/Views/Patterns/PatternWebPagesBrowseView.swift
+++ LollySwiftUIiOS/LollySwiftUIiOS/Views/Patterns/PatternWebPagesBrowseView.swift
@@ -21,6 +21,20 @@ struct PatternWebPagesBrowseView: View {
             }
             .modifier(PickerModifier(backgroundColor: Color.color2))
             WebView(webView: webViewStore.webView) {}
+            // https://stackoverflow.com/questions/60885532/how-to-detect-swiping-up-down-left-and-right-with-swiftui-on-a-view
+            .gesture(
+                DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                    .onEnded { value in
+                        if value.translation.width < 0 {
+                            // left
+                            swipe(-1)
+                        }
+                        if value.translation.width > 0 {
+                            // right
+                            swipe(1)
+                        }
+                    }
+            )
         }
         .navigationTitle("Pattern Web Pages (Browse)")
         .onAppear {
@@ -31,9 +45,14 @@ struct PatternWebPagesBrowseView: View {
         }
     }
 
-    func currentWebPageChanged() {
+    private func currentWebPageChanged() {
         AppDelegate.speak(string: vm.currentWebPage.TITLE)
         webViewStore.webView.load(URLRequest(url: URL(string: vm.currentWebPage.URL)!))
+    }
+
+    private func swipe(_ delta: Int) {
+        vm.next(delta)
+        currentWebPageChanged()
     }
 }
 
