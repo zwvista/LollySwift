10b4993201c07ffd76c2d9f3a86c2c76dcdaf1ce
趙偉 <>
2023-01-01 02:10:14 +0900

    uiview 32

--- LollySwiftUIiOS/LollySwiftUIiOS/Views/Words/WordsUnitBatchEditView.swift
+++ LollySwiftUIiOS/LollySwiftUIiOS/Views/Words/WordsUnitBatchEditView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct WordsUnitBatchEditView: View {
+    @ObservedObject var vm: WordsUnitViewModel
+    @Binding var showBatch: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct WordsBatchEditView_Previews: PreviewProvider {
-    static var previews: some View {
-        WordsUnitBatchEditView()
+        Form {
+            HStack {
+                Text("UNIT:")
+                Spacer()
+//                Picker("", selection: $vmEdit.itemEdit.indexUNIT) {
+//                    ForEach(vmSettings.arrUnits.indices, id: \.self) {
+//                        Text(vmSettings.arrUnits[$0].label)
+//                    }
+//                }
+            }
+            HStack {
+                Text("PART:")
+                Spacer()
+//                Picker("", selection: $vmEdit.itemEdit.indexPART) {
+//                    ForEach(vmSettings.arrParts.indices, id: \.self) {
+//                        Text(vmSettings.arrParts[$0].label)
+//                    }
+//                }
+            }
+            HStack {
+                Text("SEQNUM:")
+                Spacer()
+//                TextField("SEQNUM", text: $vmEdit.itemEdit.SEQNUM)
+            }
+        }
+        .navigationBarItems(leading: Button("Cancel", role: .cancel) {
+            showBatch.toggle()
+        }, trailing: Button("Done") {
+        })
     }
 }
