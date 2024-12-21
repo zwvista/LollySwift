e85f79ac2c7740d32ab9020aed3640e7042943c3
趙偉 <zwvista@msn.com>
2024-12-21 16:50:47 +0900

    main 2

---
+++ LollySwiftMac/LollySwiftMac/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  LollySwiftMac
+//
+//  Created by 趙偉 on 2024/12/21.
+//  Copyright © 2024 趙偉. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
