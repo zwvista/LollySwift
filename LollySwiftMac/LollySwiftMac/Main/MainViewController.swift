033d8aba659dbbd24bb4602a173aff0e2736ed80
趙偉 <zwvista@msn.com>
2024-12-25 23:34:38 +0900

    phrases 5

--- LollySwiftMac/LollySwiftMac/Main/MainViewController.swift
+++ LollySwiftMac/LollySwiftMac/Main/MainViewController.swift
@@ -13,10 +13,12 @@ class MainViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        showTab(storyBoardName: "Words", viewControllerID: "WordsUnitViewController", label: "Words in Unit")
-        showTab(storyBoardName: "Phrases", viewControllerID: "PhrasesUnitViewController", label: "Phrases in Unit")
-        showTab(storyBoardName: "Words", viewControllerID: "WordsTextbookViewController", label: "Words in Textbook")
-        showTab(storyBoardName: "Phrases", viewControllerID: "PhrasesTextbookViewController", label: "Phrases in Textbook")
+//        showTab(storyBoardName: "Words", viewControllerID: "WordsUnitViewController", label: "Words in Unit")
+//        showTab(storyBoardName: "Phrases", viewControllerID: "PhrasesUnitViewController", label: "Phrases in Unit")
+//        showTab(storyBoardName: "Words", viewControllerID: "WordsTextbookViewController", label: "Words in Textbook")
+//        showTab(storyBoardName: "Phrases", viewControllerID: "PhrasesTextbookViewController", label: "Phrases in Textbook")
+        showTab(storyBoardName: "Words", viewControllerID: "WordsLangViewController", label: "Words in Language")
+        showTab(storyBoardName: "Phrases", viewControllerID: "PhrasesLangViewController", label: "Phrases in Language")
     }
 
     func showTab(storyBoardName: String, viewControllerID: String, label: String) {
