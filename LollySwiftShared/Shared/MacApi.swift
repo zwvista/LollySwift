cac28f1b57f1e80ee516bddc63aeca217eda7d11
zwvista <zwvista@msn.com>
2025-07-18 15:57:26 +0900

    loadHTMLStringWithMagic
    (cherry picked from commit 93abac19ee9e63b084a46d3254ca21611450ad18)

--- LollySwiftShared/Shared/MacApi.swift
+++ LollySwiftShared/Shared/MacApi.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import WebKit
 
 class MacApi {
     static func copyText(_ text: String) {
@@ -52,5 +53,18 @@ public extension NSControl {
         for b in exposedBindings {
             unbind(b)
         }
+    }
+}
+
+extension WKWebView {
+
+    /// load HTML String same font like the UIWebview
+    ///
+    //// - Parameters:
+    ///   - content: HTML content which we need to load in the webview.
+    ///   - baseURL: Content base url. It is optional.
+    func loadHTMLStringWithMagic(content:String,baseURL:URL?){
+        let headerString = "<header><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'></header>"
+        loadHTMLString(headerString + content, baseURL: baseURL)
     }
 }
